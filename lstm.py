776b6bc8ce580b9823aac61bd3a90c1f8e86596d
JAIJANYANI <jaijanyani@gmail.com>
2017-10-26 17:47:53 +0000

    Changes!

--- lstm.py
+++ lstm.py
@@ -1,6 +1,6 @@
 # LSTM for international airline passengers problem with regression framing
 import numpy
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from pandas import read_csv
 import math
 from keras.models import Sequential
@@ -10,6 +10,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import accuracy_score
 import numpy as np
+from keras.models import model_from_json
+
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=5):
 	dataX, dataY = [], []
@@ -28,7 +30,7 @@ dataset = dataset.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 # split into train and test sets
-train_size = int(len(dataset) * 0.67)
+train_size = int(len(dataset) * 0.9)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 # reshape into X=t and Y=t+1
@@ -60,14 +62,37 @@ print("-----------------------------")
 
 
 # create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
-model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+# model = Sequential()
+# model.add(LSTM(4, input_shape=(1, look_back)))
+# model.add(Dense(1))
+# model.compile(loss='mean_squared_error', optimizer='adam')
+# model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 # make predictions
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
+
+# serialize model to JSON
+# model_json = model.to_json()
+# with open("model.json", "w") as json_file:
+#     json_file.write(model_json)
+# # serialize weights to HDF5
+# model.save_weights("model.h5")
+# print("Saved model to disk")
+ 
+# later...
+ 
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+loaded_model.compile(loss='mean_squared_error', optimizer='adam')
+
+print("Loaded model from disk")
+
+
+trainPredict = loaded_model.predict(trainX)
+testPredict = loaded_model.predict(testX)
 
 
 
@@ -99,15 +124,4 @@ testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 # print(accuracy_score(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 # shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
+
